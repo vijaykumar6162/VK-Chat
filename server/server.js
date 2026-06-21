f2be9dde4e340df7b3ac78ab82f8b01cc66d7139
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-06-21 12:28:06 +0530

    Initialize Express server with Socket.io integration
    Set up a basic Express server with Socket.io for real-time communication.

--- server/server.js
+++ server/server.js
@@ -1,1 +1,30 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("User Connected:", socket.id);
+
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected:", socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
