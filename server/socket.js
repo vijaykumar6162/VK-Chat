9500115ec71d6a033c68039a6b844eb2372252cb
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-06-21 12:29:21 +0530

    Implement socket.io connection and messaging events
    Set up socket.io event listeners for connection, joining, and messaging.

--- server/socket.js
+++ server/socket.js
@@ -1,1 +1,23 @@
+module.exports = (io) => {
 
+    io.on("connection", (socket) => {
+        console.log("User Connected:", socket.id);
+
+        socket.on("join", (username) => {
+            console.log(username + " joined");
+            io.emit("message", {
+                user: "System",
+                text: `${username} joined the chat`
+            });
+        });
+
+        socket.on("chat message", (data) => {
+            io.emit("chat message", data);
+        });
+
+        socket.on("disconnect", () => {
+            console.log("User Disconnected:", socket.id);
+        });
+    });
+
+};
